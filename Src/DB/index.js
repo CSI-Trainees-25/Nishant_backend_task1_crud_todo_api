683940fc9e21edf5f24bd69c62f040166b34d2ba
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-10-19 14:25:56 +0530

    connected database

---
+++ Src/DB/index.js
@@ -0,0 +1,15 @@
+import mongoose, { connect } from "mongoose";
+import { DB_NAME } from "../constant.js";
+
+
+ const  connectdb = async()=> {
+    try{
+         const connects= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+         console.log(`\n MongoDB connected :: DB_HOST: ${connects.connection.host}`
+    );
+        }catch (error){
+            console.error("Mongo_db_connection error:", error);
+            process.exit(1)
+      }
+ }
+ export default connectdb
--- index.js
+++ index.js
@@ -1,46 +1,14 @@
-import express from "express" 
-import mongoose from "mongoose"
-import product from "./Src/Models/product.models.js";
-const app = express() 
-const port =4000
-
-app.use(express.json()) ;
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World! nishant')
-})
-
-
-app.get('/', async(req, res) => {
- try {
-  const products=  await product.find({})
-     res.status(200).json(products);
- } catch (error) {
-     res.status(500).json({message: error.message})
- }
-})
-app.post("/api/products", async (req,res) => {
-   try {
-    const product =  await product.create(req.body);
-    res.status(200).json(product);
-   } catch (error) {
-    res.status(500).json({message: error.message})
-   }
-
-})
-
-
-
-
-mongoose.connect("mongodb+srv://nikkisinghnishant_db_user:nishant123a@nishant.2z6fume.mongodb.net")
-.then(() => {
-  console.log("database is connected !!!");
-
-  app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-})
-.catch((error) => {
-  console.error("connection failed", error);
-});
+import express from 'express' 
+ const app= express()
+ 
+ import dotenv from 'dotenv'
+dotenv.config();
+import connectdb from './Src/DB/index.js';
+connectdb()
+ 
+ app.get('/',(req,res)=>{
+   res.send('hello todo api')
+ })
+ app.listen(process.env.PORT ||9000,()=>{
+   console.log(`Server is ready at ${process.env.PORT }`)
+ })
