206b8359073205d3163d75e13ec8fccb42348889
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-10-19 19:43:06 +0530

    worked on update todos

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
   res.send('hello todo api')
 })
 
-app.use(express.json()) ;
+app.use(express.json());
 
 connectdb()
   .then(() => {
@@ -25,14 +25,14 @@ connectdb()
   });
 
 
-app.get('/api/todos',async(req,res) => {
-     try {
-       const alltodos= await Todo.find({})
-       res.status(200).json(alltodos)
-        
-     } catch (error) {
-      res.status(500).json({message:error.message})
-     }
+app.get('/api/todos', async (req, res) => {
+  try {
+    const alltodos = await Todo.find({})
+    res.status(200).json(alltodos)
+
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 })
 
 app.get('/api/todo/:id', async (req, res) => {
@@ -59,3 +59,18 @@ app.post('/api/todos', async (req, res) => {
     res.status(500).json({ message: error.message })
   }
 })
+
+app.put('/api/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const updatetodos = await Todo.findByIdAndUpdate(id, req.body, { new: true })
+    if (!updatetodos) {
+      return res.status(404).json({ message: "Todo not found" })
+    }
+    res.status(200).json(updatetodos)
+
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+})
+
